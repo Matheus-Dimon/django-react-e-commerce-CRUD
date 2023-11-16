582bfa73b36c95047ba62e950cf0baaa6ac38773
eloas77 <matheus.dimon@gmail.com>
2023-11-16 07:41:36 -0300

    urls config

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles', 
     'api.apps.ApiConfig',
-  
     'rest_framework',
     'corsheaders',
 ]
--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('api/products/',include('api.urls.products_urls')),
+    path('api/products/',include('api.urls.user_urls')),
+    path('api/products/',include('api.urls.order_urls')),
+]   
+
+urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
